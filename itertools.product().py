cf6ff5a9eb742f22be72b839d319fce90486cd29
Farhad0111 <109299643+Farhad0111@users.noreply.github.com>
2022-11-24 18:11:40 +0600

    Delete itertools.product().py

--- itertools.product().py
+++
@@ -1,9 +0,0 @@
-import itertools
-class Farhad:
-    def __init__(self):
-        self.l1=list(map(int,input().split()))
-        self.l2=list(map(int,input().split()))
-    def faru(self):
-        print(*itertools.product(self.l1,self.l2))
-farhad=Farhad()
-farhad.faru()
